92698ab63f6484a6b35cc8e9a551562ba7435677
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2022-12-16 14:43:48 +0000

    Add total months js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var finances = [
+const finances = [
 ['Jan-2010', 867884],
 ['Feb-2010', 984655],
 ['Mar-2010', 322013],
@@ -86,3 +86,38 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+// <!-- Financial Analysis
+// ----------------------------
+// Total Months: 25
+// Total: $2561231
+// Average  Change: $-2315.12
+// Greatest Increase in Profits: Feb-2012 ($1926159)
+// Greatest Decrease in Profits: Sep-2013 ($-2196167)
+//  -->
+
+// The total number of months included in the dataset.
+let months = finances.length;
+console.log(`Total Months: ${months}`);
+
+// The net total amount of Profit/Losses over the entire period.
+
+let profits = 0;
+let losses = 0;
+
+
+
+// The average of the changes in Profit/Losses over the entire period.
+
+// You will need to track what the total change in profits are from month to month and then find the average.
+// (Total/Number of months)
+
+
+
+// The greatest increase in profits (date and amount) over the entire period.
+
+
+// The greatest decrease in losses (date and amount) over the entire period.
+
+
+
